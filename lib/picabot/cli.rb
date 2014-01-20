9e0b4bb111965e33ae31f6fc307f03ed37eb0257
George <somu@so.mu>
2014-01-20 12:45:56 +0400

    Revamped CLI

--- lib/picabot/cli.rb
+++ lib/picabot/cli.rb
@@ -3,20 +3,42 @@ require 'picabot/version'
 require 'optparse'
 
 module Picabot
-  CLI = Class.new
-  def CLI.new(arguments = ARGV)
-    OptionParser.new do |o|
-      o.on('--token <hex>', 'Specify a token')                 { |token|  Storage[:token] = token       }
-      o.on('--user <name>', 'Specify a username')              { |name|   Storage[:user]  = name        }
-      o.on('--organization <name>', 'Specify an organization') { |name|   Storage[:organization] = name }
-      o.on('--no-organization', 'Delete organization info')    {          Storage[:organization] = nil  }
-      o.on('-v', '--version', 'Print Picabot version')         {          puts Picabot::VERSION; exit   }
-      o.on('-w', '--workers <number>', 'Number of workers')    { |number| @workers = number             }
-      o.on('-h', '--help') { puts o; exit }
-    end.parse!(arguments)
+  module CLI
+    def self.on(*args, &block)
+      @option_parser.on(*args, &block)
+    end
 
-    worker = -> { Picabot::Worker.new }
-    return worker[] unless @workers
-    @workers ? @workers.times { Thread.new { worker[] } } : worker[]    
+    def self.option(key, type, description)
+      on("--#{key.to_s.gsub('_','-')} <#{type}>", description) { |arg| Storage[key] = arg }
+    end
+
+    def self.spawn
+      Picabot::Worker.new
+    end
+
+    def self.new(arguments = ARGV)
+      OptionParser.new do |o|
+        o.version = Picabot::VERSION
+        @option_parser = o
+
+        on '-w', '--workers <number>', 'Number of workers' do |number|
+          @workers = number
+        end
+
+        option :commit_message, 'text', 'Commit title'
+        option :error_time, 'time', 'How many seconds to wait after an exception'
+        option :fork_time, 'time', 'How many seconds to wait after a fork API call'
+        option :id, 'number', '`since` param in http://developer.github.com/v3/repos/#list-all-public-repositories'
+        option :token, 'hex', 'Generate it at https://github.com/settings/tokens/new'
+        option :user, 'name', 'Your GitHub username'
+        option :organization, 'name', 'Organization to save forks into'
+
+        on '--no-organization', 'Delete organization info' do
+          Storage[:organization] = nil
+        end
+      end.parse! arguments
+
+      @workers ? @workers.times { Thread.new { spawn } } : spawn
+    end
   end
 end
