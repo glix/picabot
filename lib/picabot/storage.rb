64300c2a57d1d67063bd293931fd23b3c525b249
George <somu@so.mu>
2014-01-20 14:42:41 +0400

    Better storage defaults

--- lib/picabot/storage.rb
+++ lib/picabot/storage.rb
@@ -2,29 +2,38 @@ require 'moneta'
 require 'logger'
 
 module Picabot
-  Storage = Moneta.new(:YAML, file: "#{Dir.home}/picabot.yaml")
+  Storage = Moneta.new(:YAML, file: "#{Dir.home}/.picabot.yml")
   
-  Storage[:id]             ||= '0'
-  Storage[:branch]         ||= 'optimize-via-picabot'
-  Storage[:commit_message] ||= 'Optimize images via Picabot'
-  Storage[:error_time]     ||= 360
-  Storage[:fork_time]      ||= 30
+  def self.default(hash)
+    Storage[hash.keys.first] ||= hash.values.first
+  end
 
-  #Storage[:logger]         ||= STDOUT
-  #Storage[:logger_level]   ||= Logger::DEBUG
+  default queue: []
 
-  Storage[:pr_title]       ||= 'Losslessly compress images via Picabot'
-  Storage[:pr_text]        ||= "Hello! It is [**Picabot**, an automatic GitHub image optimizer](https://github.com/somu/picabot).\n\n" \
-                               "Your repository cointains some images, and I compressed them for you. " \
-                               "Do not worry: the compression is lossless and based on [ImageOptim](http://imageoptim.com).\n\n" \
-                               "The reason why this bot exists: we need a faster web. There are people who have got a " \
-                               "broadband, 4G and all the cool stuff. But still there are some countries where additional 50 kilobytes " \
-                               "take a reasonable time to download (like couple of minutes). Plus, this PR is going to save some precious " \
-                               "disk space, decrease server load, and so on. Still, if you do not need your images optimized, feel " \
-                               "free to close this pull request.\n\n" \
-                               "Drop me a line: <somu@so.mu>, especially if you think the bot bugged this time. " \
-                               "You also can donate so I could build a server which will send these precious pull requests more " \
-                               "often. And also I could dedicate all my work time to open-source, and it is my dream, in fact. \n\n" \
-                               "[![Donate](https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZXESQ83MM3H78)\n\n" \
-                               "Pull requests, forks and favs are welcome: https://github.com/somu/picabot"
+  # See description of values in cli.rb
+  default id: '0'
+  default branch: 'optimize-via-picabot'
+  default commit_message: 'Optimize images via Picabot'
+  default error_time: 30
+  default fork_time: 20
+
+  default pull_request_title: 'Losslessly compress images via Picabot'
+  default pull_request_body: <<-BODY
+Hello! It is [**Picabot**, an automatic GitHub image optimizer](https://github.com/somu/picabot).
+
+Your repository cointains some images, and I compressed them for you.
+Do not worry: the compression is lossless and uses [ImageOptim](http://imageoptim.com) toolkit.
+
+This bot exists to make the Internet faster. There are people who've got a fast internet connection,
+but there are countries where additional 50 KBs take a reasonable time to download (like additional 2-3 minutes).
+Additionally, this PR is going to save some precious disk space and decrease your server load. Picabot saves
+approximately 25% of space. Still, if you do not need your images optimized, feel free to close this pull request.
+
+If I have enough money, I'll build a server which will send these precious pull requests much faster.
+And if I had monthly donations, I would dedicate all my work time to open-source, so donations are very appreciated.
+
+[![Donate](https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=ZXESQ83MM3H78)
+
+Pull requests, forks and favs are welcome: https://github.com/somu/picabot
+BODY
 end
