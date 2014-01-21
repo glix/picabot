8bf9dfdf04f330ded3101ca1ce62ffed4ab2c718
George <somu@so.mu>
2014-01-21 11:50:10 +0400

    More consistent Repo class + API as a module

---
+++ lib/picabot/api.rb
@@ -0,0 +1,20 @@
+module Picabot
+  module API
+    GITHUB = 'https://api.github.com'
+
+    def execute(method, *args)
+      options = { method: method, url: "#{GITHUB}#{args[0]}", headers: {:Authorization => "token #{Storage[:token]}"} }
+      options[:payload] = args[1].to_json if args[1]
+      $stderr.puts "#{method.upcase} #{args[0]} #{options[:payload]}"
+      JSON.parse(RestClient::Request.execute(options), symbolize_names: true)
+    end
+
+    def get(path)
+      execute(:get, path)
+    end
+
+    def post(path, payload)
+      execute(:post, path, payload)
+    end
+  end
+end
--- lib/picabot/repo.rb
+++ lib/picabot/repo.rb
@@ -6,35 +6,25 @@ require 'git'
 
 module Picabot
   class Repo
-    GITHUB  = 'https://api.github.com'
-
-    class << self
-      def all
-        if @semaphore
-          sleep 1
-          return
-        end
-
-        @semaphore = true
-        put_to_queue "/repositories?since=#{Storage[:id]}", true
-        @semaphore = false
-      end
-
-      def user(name)
-        put_to_queue "/users/#{name}/repos"
-      end
-    end
+    include API
+    attr_reader :id
 
     def initialize(repo)
-      @repo = repo
+      @repo = repo[:full_name]
+      @fork = repo[:fork]
+      @id   = repo[:id]
     end
 
-    def clone(pattern)
-      directory = pattern % @repo
-      @directory = directory
-      @base = Git.clone(fork, directory)
-      @base.branch(Storage[:branch]).checkout
-      directory
+    def include_images?
+      get("/repos/#{@repo}/git/trees/master?recursive=1")[:tree].any? do |f|
+        f[:path] =~ /\.(jpg|png|gif)$/
+      end
+    rescue
+      false
+    end
+
+    def fork?
+      @fork
     end
 
     def fork
@@ -44,9 +34,19 @@ module Picabot
       response[:ssh_url]
     end
 
-    def proccess
+    def clone(pattern = '/tmp/picabot/%s')
+      directory = pattern % @repo
+      @directory = directory
+      @base = Git.clone(fork, directory)
+      @base.branch(Storage[:branch]).checkout
+      directory
+    end
+
+    def proccess(&block)
+      yield clone
       commit
       pull_request
+      mark_as_processed
       remove
     end
 
@@ -55,8 +55,11 @@ module Picabot
       @base.push 'origin', Storage[:branch]
     end
 
+    def location
+      Storage[:organization] || Storage[:user]
+    end
+
     def pull_request
-      location = Storage[:organization] || Storage[:user]
       post "/repos/#{@repo}/pulls", {
         title: Storage[:pull_request_title],
         body:  Storage[:pull_request_body],
@@ -65,46 +68,12 @@ module Picabot
       }
     end
 
-    def remove
-      FileUtils.rm_rf(@directory)
+    def mark_as_processed
+      Storage[:processed] <<= repo.id
     end
 
-    private
-
-    extend Forwardable
-    def_delegators self, :get, :post
-
-    class << self
-      def put_to_queue(endpoint, save_id = false)
-        get(endpoint).each do |repo|
-          begin
-            next if repo[:fork] unless $FORKS
-            files = get "/repos/#{repo[:full_name]}/git/trees/master?recursive=1"
-            if files[:tree].any? { |f| f[:path] =~ /\.(jpg|png|gif)$/ }
-              Storage[:queue] <<= new(name = repo[:full_name])
-              $stderr.puts "FOUND #{name}"
-            end
-            Storage[:id] = repo[:id] if save_id
-          rescue
-            next
-          end
-        end
-      end
-
-      def execute(method, *args)
-        options = { method: method, url: "#{GITHUB}#{args[0]}", headers: {:Authorization => "token #{Storage[:token]}"} }
-        options[:payload] = args[1].to_json if args[1]
-        $stderr.puts "#{method.upcase} #{args[0]} #{options[:payload]}"
-        JSON.parse(RestClient::Request.execute(options), symbolize_names: true)
-      end
-
-      def get(path)
-        execute(:get, path)
-      end
-
-      def post(path, payload)
-        execute(:post, path, payload)
-      end
+    def remove
+      FileUtils.rm_rf(@directory)
     end
   end
 end
